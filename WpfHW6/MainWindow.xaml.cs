b2a8a61e270821ca43f4607faba9d3d485f1734b
Aleksei Allen <aqml@mail.ru>
2021-11-11 13:23:24 +0300

    Add project files.

---
+++ WpfHW6/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHW6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt| Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllLines(saveFileDialog.SafeFileName, allText);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        WheatherControl wheatherControl = new WheatherControl();
+        public String[] allText = new String[4];
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "Температура: " + wheatherControl.TemperatureRange();
+            textBox2.Text = "Направление ветра: " + wheatherControl.WindDirection();
+            textBox3.Text = "Скорость ветра: " + wheatherControl.WindSpeed();
+            textBox4.Text = "Осадки: " + wheatherControl.WeatherPrecipit();
+            CollectData();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "Температура: " + wheatherControl.TemperatureRange();
+            textBox2.Text = "Направление ветра: " + wheatherControl.WindDirection();
+            textBox3.Text = "Скорость ветра: " + wheatherControl.WindSpeed();
+            textBox4.Text = "Осадки: " + wheatherControl.WeatherPrecipit();
+            CollectData();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "Температура: " + wheatherControl.TemperatureRange();
+            textBox2.Text = "Направление ветра: " + wheatherControl.WindDirection();
+            textBox3.Text = "Скорость ветра: " + wheatherControl.WindSpeed();
+            textBox4.Text = "Осадки: " + wheatherControl.WeatherPrecipit();
+            CollectData();
+        }
+
+        public void CollectData()
+        {
+            allText.SetValue(textBox1.Text, 0);
+            allText.SetValue(textBox2.Text, 1);
+            allText.SetValue(textBox3.Text, 2);
+            allText.SetValue(textBox4.Text, 3);
+        }
+    }
+    
+}
---
+++ WpfHW6/WheatherControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfHW6
+{
+    public class WheatherControl : DependencyObject
+    {
+        string[] windDirection = { "Северный", "Южный", "Восточный", "Западный" };
+        Random random = new Random();
+
+        public static readonly DependencyProperty.TemperatureProperty;
+
+        public enum Precipitations
+        {
+            солнечно,
+            облачно,
+            дождливо,
+            снег
+        }
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty);
+        }
+
+
+
+
+
+
+
+        //public string TemperatureRange()
+        //{
+        //    int temperatureRange = random.Next(-50, 50);
+        //    return temperatureRange.ToString();
+        //}
+
+        public string WindDirection()
+        {
+            int indexWind = random.Next(windDirection.Length);
+            return windDirection[indexWind];
+        }
+
+        public string WindSpeed()
+        {
+            int windSpeed = random.Next(0, 20);
+            return windSpeed.ToString();
+        }
+
+        public string WeatherPrecipit()
+        {
+            Type type = typeof(Precipitations);
+            Array values = type.GetEnumValues();
+            int indexPrecipit = random.Next(values.Length);
+            Precipitations value = (Precipitations)values.GetValue(indexPrecipit);
+            return value.ToString();
+        }
+    }
+}
